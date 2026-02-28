8d4917a143372e5ceba91534036b2af293c4fccd
Philemon Hini <philemonwatch@gmail.com>
2026-02-28 21:06:22 +0000

    Add Tawk.to live chat integration in layout: Included Tawk.to script for real-time customer support in the layout component. This addition enhances user engagement by providing immediate assistance options on the site.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,22 @@ export default function RootLayout({
       </head>
       <body className={`${dmSans.variable} ${playfair.variable}`}>
         {children}
+        {/* Start of Tawk.to Script */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69a35816f376451c37352509/1jij106o4';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`,
+          }}
+        />
+        {/* End of Tawk.to Script */}
       </body>
     </html>
   );
